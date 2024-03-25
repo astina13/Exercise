2c737307f0f3d581f1057cda45d96b9c65a8defe
astina13 <kunastmr@gmail.com>
2024-03-25 20:45:26 +0530

    javascript

---
+++ Javascript/javascript.js
@@ -0,0 +1,67 @@
+console.log("hello javascript")
+console.log(1+2)
+
+//const, var, let
+const PI = 3.1415;
+
+var x = 1;
+console.log(x)
+var y = 1.4;
+console.log(y)
+var z = "hello";
+console.log(z)
+var a = [ x, y, z];
+console.log(a)
+var lst = [1, "0" [1, 2], 4.8, true];
+console.log(lst)
+var obj ={
+    name: "Mr X",
+    job: "hacker",
+    age: 23,
+    address: "bengaluru",
+}
+console.log(obj)
+
+let phone = {
+    company: {
+        name: "samsung",
+        countery: " soouth korea",
+    },
+    name: "galaxy",
+    size: 7.4,
+    weight: 200,
+    color: ['blue', 'red', 'green']
+}   
+
+let str1 =`hello`;
+console.log(str1)
+let str2 = `world`;
+console.log(str2)
+let str3 = `hello
+world`;
+console.log(str3)
+
+let outcome = [[1, 3, 2, 4, 5, 6],
+               [4, 2, 1, 5, 3, 6],
+               [1, 6, 5, 2, 4, 3],
+               [4, 3, 1, 2, 6, 5],
+               [6, 5, 3, 8, 2, 1],
+               [4, 5, 6, 1, 2, 3]]
+console.log(outcome[1][3])//5
+console.log(outcome[4][3])//8
+
+let D3 = [
+          [
+            [1, 2],
+            [1, 2]
+            ],
+          [
+            [1, 2],
+            [1, 2]
+            ],
+          [
+            [1, 2],
+            [1, 2]
+            ],
+        ]
+        //3 * 2
