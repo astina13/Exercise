f7d777abb76b027e21774cbc74d122a7c15a39c1
astina13 <kunastmr@gmail.com>
2024-03-28 14:37:12 +0530

    javascript.js

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -65,3 +65,39 @@ let D3 = [
             ],
         ]
         //3 * 2
+
+
+        //arithmetic operations
+let sum = 1 + 1.4; // 2.4
+let dif = 1 - 1.4; // -0.4
+let mul = 2 * 4; // 8
+let div = 4 / 2; // 2
+let mod = 5 % 2; // 1
+let pow = 5 ** 2; // 25
+let ex1 = 2 * (1 - 6 / 2); // -4
+
+
+//boolean
+let isMore = 2 >= 4; // false
+let isLess = 5 < 10; // true
+let bool1 = isMore || isLess; // true
+let bool2 = isMore && isLess; // false
+let isEqual1 = 0 == '0';//true type conversion
+let isEqual2 = 0 === '0'; //false no type conversion
+let notEqual1 = 0 != '0'; //false
+let notEqual2 = 0 !== '0'; //true
+
+
+//string
+let str4 = 'Hello';
+let str5 = 'World';
+let str6 = str1 + ' ' + str2;//'Hello World'
+let num = 42;
+let str7 = `The answer to universe is ${num}.`;
+
+//arrays
+let arr1 = [1, 2];
+let arr2 = [3, 4];
+let arr3 = arr1 + arr2; // '1, 23, 4'
+// ... spread operator
+let arr4 = [...arr1, ...arr2];// [1, 2, 3, 4]
